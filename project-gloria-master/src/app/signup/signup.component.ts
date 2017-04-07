c7ff0e510912f2a107a707be73fe87fd71ebdc6e
chvillas <cristian.9077@hotmail.com>
2017-04-07 16:17:14 -0500

    Validation
    two fields password

--- project-gloria-master/src/app/signup/signup.component.ts
+++ project-gloria-master/src/app/signup/signup.component.ts
@@ -9,33 +9,51 @@ import { IUser, User } from '../models/user';
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css'],
   animations: [moveIn(), fallIn()],
-  host: {'[@moveIn]': ''}
+  host: { '[@moveIn]': '' }
 })
 export class SignupComponent implements OnInit {
-  
- state: string = '';
+
+  state: string = '';
   error: any;
 
-  constructor(public af: AngularFire,private router: Router) {
+  noMatch: boolean = false;
+  password_init = "";
+  passAreEqual = false;
+
+  constructor(public af: AngularFire, private router: Router) {
 
   }
 
+  validate(event: any) {
+    if (event.target.value == this.password_init) {
+      this.passAreEqual = true;
+      alert("Same passwords");
+    } else {
+      this.passAreEqual = false;
+    }
+  }
+
   onSubmit(formData) {
-    if(formData.valid) {
-      console.log(formData.value);
-      this.af.auth.createUser({
-        email: formData.value.email,
-        password: formData.value.password
-      }).then(
-        (success) => {
-        console.log(success);
-        this.router.navigate(['/login']);
-         this.af.database.object('users/'+success.uid ).set(new User(formData.value.namef));
-      }).catch(
-        (err) => {
-        console.log(err);
-        this.error = err;
-      })
+    if (formData.valid) {
+      if (this.passAreEqual) {
+        console.log(formData.value);
+        this.af.auth.createUser({
+          email: formData.value.email,
+          password: formData.value.password
+        }).then(
+          (success) => {
+            console.log(success);
+            this.router.navigate(['/login']);
+            this.af.database.object('users/' + success.uid).set(new User(formData.value.namef));
+          }).catch(
+          (err) => {
+            console.log(err);
+            this.error = err;
+          })
+      } else {
+        this.noMatch = true;
+        alert("Passwords do not consider")
+      }
     }
   }
   ngOnInit() {
