ac5aa902afda163b167222d1e8885d983b7cc59c
chvillas <cristian.9077@hotmail.com>
2017-04-27 13:49:40 -0500

    comentario

--- project-gloria-master/src/app/app.conection.ts
+++ project-gloria-master/src/app/app.conection.ts
@@ -1,5 +1,5 @@
 import { CanActivate, Router } from '@angular/router';
-import { AngularFireAuth, AngularFire, FirebaseListObservable, FirebaseApp  } from "angularfire2/angularfire2";
+import { AngularFireAuth, AngularFire, FirebaseListObservable, FirebaseApp } from "angularfire2/angularfire2";
 import { Injectable, Inject } from "@angular/core";
 import { Observable } from "rxjs/Rx";
 import 'rxjs/add/operator/do';
@@ -8,75 +8,76 @@ import 'rxjs/add/operator/take';
 import * as firebase from 'firebase';
 
 @Injectable()
-export class QueryDB  {
+export class QueryDB {
     fileList: FirebaseListObservable<any[]>;
     anyList: Observable<any[]>;
-    firebase:any;
+    firebase: any;
     constructor(private auth: AngularFireAuth, private router: Router, public af: AngularFire, @Inject(FirebaseApp) firebase: any) {
         this.firebase = firebase;
     }
 
-    listProductForClient(url: string ): any {
+    listProductForClient(url: string): any {
         let storage = firebase.storage();
         // Create a storage reference from our storage service
         var storageRef = storage.ref();
-        this.fileList =  this.af.database.list(url);
-        this.anyList =  this.fileList.map(itemList =>
-          itemList.map(item => {
-            // Child references can also take paths delimited by '/'
-            var spaceRef = storageRef.child(item.path);
-            var errors= false;
-            // Get the download URL
-            var imge =spaceRef.getDownloadURL().then( (url) => {
-            // Insert url into an <img> tag to "download"
-            console.log(url);
-            return url;
-            }).catch((error: any) => {
-            switch (error.code) {
-                    case 'storage/object_not_found':
-                    // File doesn't exist
-                    console.log(error);
-                    errors = true;
-                    break;
+        this.fileList = this.af.database.list(url);
+        this.anyList = this.fileList.map(itemList =>
+            itemList.map(item => {
+                // Child references can also take paths delimited by '/'
+                var spaceRef = storageRef.child(item.path);
+                var errors = false;
+                // Get the download URL
+                var imge = spaceRef.getDownloadURL().then((url) => {
+                    // Insert url into an <img> tag to "download"
+                    return url;
+                }).catch((error: any) => {
+                    switch (error.code) {
+                        case 'storage/object_not_found':
+                            // File doesn't exist
+                            console.log(error);
+                            errors = true;
+                            break;
 
-                    case 'storage/unauthorized':
-                    // User doesn't have permission to access the object
-                    console.log(error);
-                    errors = true;
-                    break;
+                        case 'storage/unauthorized':
+                            // User doesn't have permission to access the object
+                            console.log(error);
+                            errors = true;
+                            break;
 
-                    case 'storage/canceled':
-                    // User canceled the upload
-                    console.log(error);
-                    errors = true;
-                    break;
+                        case 'storage/canceled':
+                            // User canceled the upload
+                            console.log(error);
+                            errors = true;
+                            break;
 
-                    
 
-                    case 'storage/unknown':
-                    // Unknown error occurred, inspect the server response
-                    console.log(error);
-                    errors = true;
-                    break;
+
+                        case 'storage/unknown':
+                            // Unknown error occurred, inspect the server response
+                            console.log(error);
+                            errors = true;
+                            break;
                     }
                 });
-            //var pathReference = storage.ref(item.path).getDownloadURL().then(url => { return url });
-            let result = { $key: item.$key, downloadURL: imge , path: item.path, product: item.product, price: item.price, quantity: item.quantity };
-            
-            return result;
-          })
+               
+                //var pathReference = storage.ref(item.path).getDownloadURL().then(url => { return url });
+                let result = { $key: item.$key, downloadURL: imge, path: item.path, product: item.product, price: item.price, quantity: item.quantity };
+
+                return result;
+            })
         );
         return this.anyList;
     }
-    db_af_ref(url: string ): any{
-       return  firebase.database().ref(url);
+
+    db_af_ref(url: string): any {
+        return firebase.database().ref(url);
     }
-    getData(url: string){
-          var quantityProduct = firebase.database().ref(url);
-          quantityProduct.on('value', snapshot => { quantityProduct = snapshot.val(); }); 
-          return quantityProduct;    
+    getData(url: string) {
+        var quantityProduct = firebase.database().ref(url);
+        quantityProduct.on('value', snapshot => { quantityProduct = snapshot.val(); });
+        return quantityProduct;
     }
-    removeItem(url: string, key: string): any{
+    removeItem(url: string, key: string): any {
         const transaction = this.af.database.list(url);
         transaction.remove(key);
     }
--- project-gloria-master/src/app/inventory/inventory.component.ts
+++ project-gloria-master/src/app/inventory/inventory.component.ts
@@ -8,12 +8,12 @@ import { Observable } from 'rxjs';
 
 
 interface Image {
-    path: string;
-    product?: string;
-    price?: any;
-    quantity?: any;
-    downloadURL?: string;
-    $key?: string;
+  path: string;
+  product?: string;
+  price?: any;
+  quantity?: any;
+  downloadURL?: string;
+  $key?: string;
 
 }
 @Component({
@@ -22,183 +22,208 @@ interface Image {
   styleUrls: ['./inventory.component.css']
 })
 export class InventoryComponent implements OnInit {
-   
-   public ObjProd: Object[] = [];
-    public ObjImg: FirebaseListObservable<any[]>;
-   //image gestor
 
-    @Input() folder: string;
+  public ObjProd: Object[] = [];
+  public ObjImg: FirebaseListObservable<any[]>;
+  //image gestor
+
+  @Input() folder: string;
+
+  fileList: FirebaseListObservable<Image[]>;
+  imageList: Observable<Image[]>;
+  priceTotals: number = 0;
+  constructor(public af: AngularFire, private router: Router) {
+    let storage = firebase.storage();
+    this.ObjProd["path"] = " ";
+    this.fileList = this.af.database.list(`/inventory`);
+    this.imageList = this.fileList.map(itemList =>
+      itemList.map(item => {
+        var pathReference = storage.ref(item.path).getDownloadURL().then(url => { return url });
+        let result = { $key: item.$key, downloadURL: pathReference, path: item.path, product: item.product, price: item.price, quantity: item.quantity };
+        return result;
+      })
+    );
     
-    fileList : FirebaseListObservable<Image[]>;
-    imageList : Observable<Image[]>;
-  constructor(public af: AngularFire,private router: Router) {
-        let storage = firebase.storage();
-      this.ObjProd["path"] = " ";
-        this.fileList = this.af.database.list(`/inventory`);
-        this.imageList = this.fileList.map( itemList =>
-            itemList.map( item => {
-                var pathReference = storage.ref(item.path).getDownloadURL().then(url => {  return url });
-                let result = {$key: item.$key, downloadURL: pathReference, path: item.path, product: item.product, price: item.price, quantity: item.quantity};
-                return result;
-            })
-        );
-
-   }
-
-
-//   delReg(id_prdc){
-//      this.items.remove(id_prdc);
-//   }
-  openModal(idProduc, type){
-      console.log(idProduc);
-    if(type){
-           this.ObjProd["key"] = idProduc;
-           
-           this.ObjProd["type"] = type;
-          this.af.database.list('/inventory/'+idProduc, { preserveSnapshot: true})
-          .subscribe(snapshots=>{
-            snapshots.forEach(snapshot => {
-              this.ObjProd[snapshot.key] = snapshot.val();
-              if(snapshot.key != "path"){
-                 this.ObjProd[snapshot.key] = snapshot.val();
-              }else{
-                    const storageRef = firebase.storage().ref().child(snapshot.val());
-                    storageRef.getDownloadURL().then(url =>  this.ObjProd["img"]  = url);
-              }
-            });
-          });
-         
-    }else{
-         const storageRef = firebase.storage().ref().child('images/noimg.jpg');
-         storageRef.getDownloadURL().then(url =>  this.ObjProd["img"]  = url);
-           this.ObjProd["key"] = idProduc;
-           this.ObjProd["type"] = type;
-           delete this.ObjProd["price"];
-           delete this.ObjProd["quantity"];
-           delete this.ObjProd["path"];
-           delete this.ObjProd["product"];
-    }
+    this.af.database.list("/users").subscribe(userList => {
+     this.priceTotals = 0;  
+      userList.map(user => {
+        this.af.database.list('/purchased/' + user.$key).subscribe((dates) => {
+                   
+          dates.map((date) => {
+            
+            this.af.database.list('/purchased/' + user.$key + '/' + date.$key).subscribe((transactions) => {
+              transactions.map((transaction) => {
+                if (transaction.price && !transaction.Ispaid) {
+                   this.priceTotals += parseFloat(transaction.price) * transaction.quantity;
+                }
+              });
           
-    
+            })
+            
+          })
+          
+        });
+        
+      });
+    });
+  
+
+  }
+
+
+  //   delReg(id_prdc){
+  //      this.items.remove(id_prdc);
+  //   }
+  openModal(idProduc, type) {
+    console.log(idProduc);
+    if (type) {
+      this.ObjProd["key"] = idProduc;
+
+      this.ObjProd["type"] = type;
+      this.af.database.list('/inventory/' + idProduc, { preserveSnapshot: true })
+        .subscribe(snapshots => {
+          snapshots.forEach(snapshot => {
+            this.ObjProd[snapshot.key] = snapshot.val();
+            if (snapshot.key != "path") {
+              this.ObjProd[snapshot.key] = snapshot.val();
+            } else {
+              const storageRef = firebase.storage().ref().child(snapshot.val());
+              storageRef.getDownloadURL().then(url => this.ObjProd["img"] = url);
+            }
+          });
+        });
+
+    } else {
+      const storageRef = firebase.storage().ref().child('images/noimg.jpg');
+      storageRef.getDownloadURL().then(url => this.ObjProd["img"] = url);
+      this.ObjProd["key"] = idProduc;
+      this.ObjProd["type"] = type;
+      delete this.ObjProd["price"];
+      delete this.ObjProd["quantity"];
+      delete this.ObjProd["path"];
+      delete this.ObjProd["product"];
+    }
+
+
   }
   ngOnInit() {
   }
-  SubmitData(formData, path= null ) {
-     var product= this.validFieldForm(formData.productN, "product" );
-     var price= this.validFieldForm(formData.price, "price" );
-     var quantity= this.validFieldForm(formData.quantity, "quantity" );
-     var pat = path == null ? this.ObjProd["path"] : path;
+  SubmitData(formData, path = null) {
+    var product = this.validFieldForm(formData.productN, "product");
+    var price = this.validFieldForm(formData.price, "price");
+    var quantity = this.validFieldForm(formData.quantity, "quantity");
+    var pat = path == null ? this.ObjProd["path"] : path;
 
-        if(this.ObjProd["type"]){
-          console.log(this.ObjProd["key"], product, price, quantity, pat)
-        this.fileList.update(this.ObjProd["key"], new Inventory(product, price, quantity, pat));
+    if (this.ObjProd["type"]) {
+      console.log(this.ObjProd["key"], product, price, quantity, pat)
+      this.fileList.update(this.ObjProd["key"], new Inventory(product, price, quantity, pat));
 
-        }else if(this.ObjProd["type"] == false){
-        this.fileList.push(new Inventory(product, price, quantity, pat));
+    } else if (this.ObjProd["type"] == false) {
+      this.fileList.push(new Inventory(product, price, quantity, pat));
 
-      }
-      return false
     }
+    return false
+  }
 
-  validFieldForm(value, Idnty){
-    if(value){
-     
+  validFieldForm(value, Idnty) {
+    if (value) {
+
       return value;
-    }else{
+    } else {
 
       return this.ObjProd[Idnty];
     }
   }
-    ngOnChanges() {
-        console.log("new values for folder");
-        let storage = firebase.storage();
-        
-        this.fileList = this.af.database.list(`/images`);
-        console.log("Rendering all images in ",`/images`)
-        this.imageList = this.fileList.map( itemList =>
-            itemList.map( item => {
-                var pathReference = storage.ref(item.path);
-                let result = {$key: item.$key, downloadURL: pathReference.getDownloadURL(), path: item.path, product: item.product};
-                console.log(result);
-                return result;
-            })
-        );
-    }
-   onSubmit(formData) {
-        // Create a root reference
-        let storageRef = firebase.storage().ref();
+  ngOnChanges() {
+    console.log("new values for folder");
+    let storage = firebase.storage();
 
-        let success = false;
-        // This currently only grabs item 0, TODO refactor it to grab them all
-        if([(<HTMLInputElement>document.getElementById('file')).files[0]][0] &&  !this.ObjProd["type"]){
-         for (let selectedFile of [(<HTMLInputElement>document.getElementById('file')).files[0]]) {
-             console.log(selectedFile);
-              //Make local copies of services because "this" will be clobbered
-             let router = this.router;
-             let af = this.af;
-             let folder = this.folder;
-             let path = `/images/${selectedFile.name}`;
-             var iRef = storageRef.child(path);
-             iRef.put(selectedFile).then((snapshot) => {
-                 this.SubmitData(formData,path);
+    this.fileList = this.af.database.list(`/images`);
+    console.log("Rendering all images in ", `/images`)
+    this.imageList = this.fileList.map(itemList =>
+      itemList.map(item => {
+        var pathReference = storage.ref(item.path);
+        let result = { $key: item.$key, downloadURL: pathReference.getDownloadURL(), path: item.path, product: item.product };
+        console.log(result);
+        return result;
+      })
+    );
+  }
+  onSubmit(formData) {
+    // Create a root reference
+    let storageRef = firebase.storage().ref();
 
-             });
-         }
-        }else{
-          if([(<HTMLInputElement>document.getElementById('file')).files[0]][0] ){
-              for (let selectedFile of [(<HTMLInputElement>document.getElementById('file')).files[0]]) {
-                  console.log(selectedFile);
-                  //delete the last path 
-                  firebase.storage().ref().child(this.ObjProd["path"] ).delete().then(
-                        () => {},
-                        (error) => console.error("Error deleting stored file",this.ObjProd["path"] )
-                    );
-                      //Make local copies of services because "this" will be clobbered
-                  let router = this.router;
-                  let af = this.af;
-                  let folder = this.folder;
-                  let path = `/images/${selectedFile.name}`;
-                  var iRef = storageRef.child(path);
-                  iRef.put(selectedFile).then((snapshot) => {
-                      this.SubmitData(formData,path);
+    let success = false;
+    // This currently only grabs item 0, TODO refactor it to grab them all
+    if ([(<HTMLInputElement>document.getElementById('file')).files[0]][0] && !this.ObjProd["type"]) {
+      for (let selectedFile of [(<HTMLInputElement>document.getElementById('file')).files[0]]) {
+        console.log(selectedFile);
+        //Make local copies of services because "this" will be clobbered
+        let router = this.router;
+        let af = this.af;
+        let folder = this.folder;
+        let path = `/images/${selectedFile.name}`;
+        var iRef = storageRef.child(path);
+        iRef.put(selectedFile).then((snapshot) => {
+          this.SubmitData(formData, path);
 
-                  });
-              }
-          }else{
-            this.SubmitData(formData);
-          }
-
-        }
-      return false
-    }
-    delReg(a, k) {
-        let storagePath = a;
-        let referencePath = k;
-        
-         const items = this.af.database.list('/inventory');
-         items.remove(referencePath);
-
-        //  delete from storage
-         firebase.storage().ref().child(storagePath).delete().then(
-              () => {},
-              (error) => console.error("Error deleting stored file",storagePath)
+        });
+      }
+    } else {
+      if ([(<HTMLInputElement>document.getElementById('file')).files[0]][0]) {
+        for (let selectedFile of [(<HTMLInputElement>document.getElementById('file')).files[0]]) {
+          console.log(selectedFile);
+          //delete the last path 
+          firebase.storage().ref().child(this.ObjProd["path"]).delete().then(
+            () => { },
+            (error) => console.error("Error deleting stored file", this.ObjProd["path"])
           );
+          //Make local copies of services because "this" will be clobbered
+          let router = this.router;
+          let af = this.af;
+          let folder = this.folder;
+          let path = `/images/${selectedFile.name}`;
+          var iRef = storageRef.child(path);
+          iRef.put(selectedFile).then((snapshot) => {
+            this.SubmitData(formData, path);
 
-    
-        // Do these as two separate steps so you can still try delete ref if file no longer exists
-
-        // Delete from Storage
-        // firebase.storage().ref().child(storagePath).delete()
-        // .then(
-        //     () => {},
-        //     (error) => console.error("Error deleting stored file",storagePath)
-        // );
-
-        // Delete references
-        // this.af.database.object(referencePath).remove()
-            
-        
+          });
+        }
+      } else {
+        this.SubmitData(formData);
+      }
 
     }
+    return false
+  }
+  delReg(a, k) {
+    let storagePath = a;
+    let referencePath = k;
+
+    const items = this.af.database.list('/inventory');
+    items.remove(referencePath);
+
+    //  delete from storage
+    firebase.storage().ref().child(storagePath).delete().then(
+      () => { },
+      (error) => console.error("Error deleting stored file", storagePath)
+    );
+
+
+    // Do these as two separate steps so you can still try delete ref if file no longer exists
+
+    // Delete from Storage
+    // firebase.storage().ref().child(storagePath).delete()
+    // .then(
+    //     () => {},
+    //     (error) => console.error("Error deleting stored file",storagePath)
+    // );
+
+    // Delete references
+    // this.af.database.object(referencePath).remove()
+
+
+
+  }
 
 }
