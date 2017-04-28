13e9b4cfae329aebec78972674f8d9d5ecc7e845
H4isan <harlengiraldo@gmail.com>
2017-04-28 07:59:01 -0500

    js fixed
    todos los js --> modales, y otras funciones

--- project-gloria-master/src/assets/js/Animation.js
+++ project-gloria-master/src/assets/js/Animation.js
@@ -13,31 +13,4 @@ $(document).ready(function () {
 
 });
 
-$(document).ready(function () {
-    $('.datepicker').pickadate({
-        format: 'mm/dd/yyyy',
-        selectMonths: true, // Creates a dropdown to control month
-        selectYears: 15 // Creates a dropdown of 15 years to control year
-    });
-     $('.button-collapse').sideNav();
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || $(window).width() < 600) {
 
-        $(document).ready(function () {
-
-            $("#test1").attr("id", "test-swipe-1");
-            $("#test2").attr("id", "test-swipe-2");
-            $('ul.tabs').tabs();
-
-        });
-      
-
-    } else {
-
-    }
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal').modal({
-        ready: function (modal, trigger) {    },
-        complete: function () { } // Callback for Modal close
-
-    });
-});
